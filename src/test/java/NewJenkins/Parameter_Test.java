9787081334e6822ce8ea35f03b22737ee0ea8a77
mujeebmujju <Mujeeburrehaman94>
2023-11-30 13:19:51 +0530

    1st commit

---
+++ src/test/java/NewJenkins/Parameter_Test.java
@@ -0,0 +1,46 @@
+package NewJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Parameter_Test 
+{
+	WebDriver driver;
+	@Test
+	public void Test()
+	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
+		System.out.println("BROWSER");
+		System.out.println("URL");
+
+		
+		if (BROWSER.equals("chrome")) 
+		{
+			driver=new ChromeDriver();
+			
+		}
+		
+		else if (BROWSER.equals("firefox")) 
+		{
+			driver=new FirefoxDriver();
+
+		} 
+		
+		else if (BROWSER.equals("edge")) 
+		{
+			driver=new EdgeDriver();
+
+			
+		}
+		
+		driver.get(URL);
+
+		
+	}
+
+}
